a01c2f78a3bd64ecca42c6284995fec690fe5d84
egoitzaulestia <egoitzaulestia@gmail.com>
2025-05-16 16:14:34 +0200

    ej2: logplease done

---
+++ ej2/index.js
@@ -0,0 +1,7 @@
+const Logger = require("logplease");
+const logger = Logger.create("utils");
+
+logger.debug("Hello World!");
+logger.info("Breaking news! I love Node.js!");
+logger.warn("Using warn method...");
+logger.error("There is something wrong!!!");
